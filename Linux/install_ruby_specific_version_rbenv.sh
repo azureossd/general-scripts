31fdc88323b17c618f6d5d7d7a85efe106b063e1
Edison Garcia <egachi@live.com>
2021-01-07 02:22:32 -0600

    Adding more recommendations for libssl and openssl issues

--- Linux/install_ruby_specific_version_rbenv.sh
+++ Linux/install_ruby_specific_version_rbenv.sh
@@ -5,7 +5,8 @@
 export RUBY_VERSION=2.2.6
 
 # Installing dependencies
-apt update && apt install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libsqlite3-dev
+apt update
+apt install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libsqlite3-dev
 
 # Copying rbenv - rbenv and ruby-build repositories from GitHub to ~/.rbenv directory.
 curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | bash -
@@ -21,3 +22,7 @@ rbenv global $RUBY_VERSION
 
 # Checking Ruby version
 ruby -v
+
+# In case it fails with The Ruby openssl extension was not compiled used the following config.
+# export RUBY_CONFIGURE_OPTS="--with-openssl-dir=/usr/lib/ssl"
+# Or install this library apt install libssl1.0-dev
