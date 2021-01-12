22391028a7aa93e0418a6186751dccb677e07cf5
Edison Garcia <egachi@live.com>
2021-01-12 13:17:25 -0600

    Updating

--- Linux/install_ruby_specific_version_rbenv.sh
+++ Linux/install_ruby_specific_version_rbenv.sh
@@ -4,38 +4,33 @@
 # Prerequisites -  NodeJS 
 
 u="$USER"
-echo "User name $u"
+echo "#####User name $u #####"
 sudo usermod -aG sudo $u
 
 h=$HOME
-logpath=$h . "/deployment_log.txt"
-echo $logpath
-touch $logpath
-chmod ugo+rwx $logpath
-sudo chmod 775 $logpath
+echo "##### Using $HOME #####"
 
-sudo echo "##### Installing NodeJS #####" >> $logpath
 # Installing nodejs
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - >> $logpath
-sudo apt-get install -y nodejs >> $logpath
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - 
+sudo apt-get install -y nodejs 
 
 # Set a specific Ruby version
 export RUBY_VERSION=2.6.2 
 
-sudo echo "##### Installing Dependencies #####" >> $logpath
+echo "##### Installing Dependencies #####" 
 # Installing dependencies
-sudo apt update >> $logpath
-sudo apt install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libsqlite3-dev >> $logpath
+sudo apt update 
+sudo apt install -y git curl libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libsqlite3-dev 
 
 # Copying rbenv - rbenv and ruby-build repositories from GitHub to ~/.rbenv directory.
-curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | sudo bash - >> $logpath
+curl -sL https://github.com/rbenv/rbenv-installer/raw/master/bin/rbenv-installer | sudo bash - 
 
 # Adding rbenv bin directory to user PATH
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc 
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 source ~/.bashrc
 
-sudo echo "##### Installing Ruby and set it global #####" >> $logpath
+echo "##### Installing Ruby and set it global #####" 
 # Install Ruby version and set it to default version
 sudo rbenv install $RUBY_VERSION >> $logpath
 sudo rbenv global $RUBY_VERSION >> $logpath
